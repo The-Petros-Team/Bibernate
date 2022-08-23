792063279623e41d2c50608af8351e464d2f484a
v.romantsev <v.romantsev@ocado.com>
2022-08-23 09:35:10 +0300

    * added scanner utility class

---
+++ bibernate-core/src/main/java/com/bobocode/petros/bibernate/utils/PackageScannerUtils.java
@@ -0,0 +1,14 @@
+package com.bobocode.petros.bibernate.utils;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.Set;
+
+@UtilityClass
+public class PackageScannerUtils {
+
+    public Set<Class<?>> scan(final Set<String> entityPackages) {
+        // TODO
+        return null;
+    }
+}
