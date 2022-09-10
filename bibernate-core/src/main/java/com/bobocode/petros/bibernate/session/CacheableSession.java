1ef8c2b5083f17cec862133b9d53b3c483edf712
Kirill Kotenok <kirill.kotenok@data-ox.com>
2022-09-10 09:46:00 +0300

    Swap line 77, 78

--- bibernate-core/src/main/java/com/bobocode/petros/bibernate/session/CacheableSession.java
+++ bibernate-core/src/main/java/com/bobocode/petros/bibernate/session/CacheableSession.java
@@ -74,7 +74,7 @@ public class CacheableSession extends DefaultSession {
 
     @Override
     public void close() {
-        persistenceContext.clear();
         super.close();
+        persistenceContext.clear();
     }
 }
